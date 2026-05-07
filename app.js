22393491c348195b9abffd4b7aecdf6ecf8d8d12
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-05-06 21:00:59 -0700

    basic display

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import path from "node:path";
 import express from "express";
 const __dirname = import.meta.dirname;
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,7 @@ app.set("view engine", "ejs");
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-
+app.use("/", indexRouter);
 
 app.listen(PORT, (error) => {
     if (error) throw error;
---
+++ routes/indexRouter.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const indexRouter = Router();
+
+const sampleMessages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: sampleMessages });
+});
+
+export default indexRouter;
