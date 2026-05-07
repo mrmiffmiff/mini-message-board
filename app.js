2190e7a66a14c106dbe8c9aa12735dca49640182
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-05-06 18:27:55 -0700

    express and ejs install with basic setup

---
+++ app.js
@@ -0,0 +1,19 @@
+import path from "node:path";
+import express from "express";
+const __dirname = import.meta.dirname;
+
+const app = express();
+const PORT = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+
+
+app.listen(PORT, (error) => {
+    if (error) throw error;
+    console.log("Listening on port 3000");
+});
